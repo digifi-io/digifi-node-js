49f9e1c2c44739111fc71a5504203db1db60264e
katerina-zaitsava <katerina.zaitsava@satelliteinnovations.io>
2022-04-13 14:19:37 +0300

    ND-2078 Update AuthApiClient constructor

--- src/AuthApiClient.ts
+++ src/AuthApiClient.ts
@@ -2,16 +2,14 @@ import { AuthorizedApiClient } from './AuthorizedApiClient';
 import { FetchOptions, HTTP_METHOD, RequestBody } from './ApiClient';
 
 class AuthApiClient extends AuthorizedApiClient {
-  protected entity;
 
   constructor(
     baseUrl: string,
     clientId: string,
     clientSecret: string,
-    entity: 'borrowers' | 'intermediaries',
+    protected entity: 'borrowers' | 'intermediaries',
   ) {
     super(baseUrl, clientId, clientSecret);
-    this.entity = entity;
   }
 
   public async makeCall<ResBody, ReqBody extends RequestBody = RequestBody>(
