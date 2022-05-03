5d403a72dbd2466c45a91d71116a58e59c4635f1
ilya-tishonok <ilya.tishonok@satelliteinnovations.io>
2022-05-03 23:11:36 +0300

    Change interface for tasks api, add bulk create

--- src/api/TasksApi.ts
+++ src/api/TasksApi.ts
@@ -1,6 +1,6 @@
 import { SystemApi } from './SystemApi';
 import { BorrowerType } from './BorrowersApi';
-import { VariableValue, UserShortInfo } from '../types';
+import { VariableValue } from '../types';
 
 export enum TaskStatus {
   Done = 'Done',
@@ -8,31 +8,106 @@ export enum TaskStatus {
   InReview = 'In Review',
 }
 
+export enum TaskAssigneeType {
+  Borrower = 'borrower',
+  Intermediary = 'intermediary',
+  Organization = 'organization',
+}
+
+export interface TaskAssignedBorrower {
+  id: string;
+  type: BorrowerType;
+  variables: Record<string, VariableValue>;
+}
+
+export interface TaskAssignedIntermediary {
+  id: string;
+  variables: Record<string, VariableValue>;
+}
+
+export interface TaskApplication {
+  id: string;
+  displayId: string;
+  borrower: {
+    id: string;
+    type: BorrowerType;
+  };
+  teamMembers: string[];
+  variables: Record<string, VariableValue>;
+}
+
 export interface Task {
   id: string;
+  description: string;
+  status: TaskStatus;
+  assigneeType: TaskAssigneeType;
+  application: TaskApplication;
+  organizationId: string;
+  group?: string;
+  autoPassCondition?: {
+    formula: string;
+    formulaRequiredVariables: string[];
+  } | null;
+  assignedBorrower?: TaskAssignedBorrower | null;
+  assignedIntermediary?: TaskAssignedIntermediary | null;
+  variablesIds?: string[] | null;
+  blockedStatuses?: Array<{
+    id: string;
+    name: string;
+  }> | null;
+  dueDate?: string | null;
   createdAt: Date;
   updatedAt: Date;
-  dueDate: string;
-  description: string;
-  teamMembers: UserShortInfo[];
-  status: TaskStatus;
-  application: {
-    id: string;
-    displayId: number;
-    variables: Record<string, VariableValue>;
-    borrowerType: BorrowerType;
-  } | null;
+}
+
+export type TaskAssignee = {
+  assigneeType: TaskAssigneeType.Borrower;
+  assigneeId: string;
+} | {
+  assigneeType: TaskAssigneeType.Intermediary;
+  assigneeId: string;
+} | {
+  assigneeType: TaskAssigneeType.Organization;
 }
 
 export interface CreateTaskParams {
-  teamMemberIds?: string[];
-  applicationId?: string;
+  application: string;
   description: string;
-  dueDate: string;
-  status: TaskStatus;
+  assignee: TaskAssignee;
+  autoPassCondition?: string | null;
+  group?: string | null;
+  dueDate?: string | null;
+  variablesIds?: string[] | null;
+  blockedStatuses?: string[] | null;
+  uniqSystemIdentifier?: string;
 }
 
-export default class TasksApi extends SystemApi<Task, CreateTaskParams, Partial<CreateTaskParams>> {
+export interface UpdateTaskParams {
+  assignee?: TaskAssignee;
+  description?: string;
+  status?: TaskStatus;
+  autoPassCondition?: string | null;
+  dueDate?: string | null;
+  group?: string | null;
+  variablesIds?: string[] | null;
+  blockedStatuses?: string[] | null;
+}
+
+export interface BulkCreateTasksParams {
+  application: string;
+  batch: Array<Omit<CreateTaskParams, 'application'>>;
+}
+
+export interface BulkCreateTasksResponse {
+  count: number;
+  batch: Task[];
+}
+
+export default class TasksApi extends SystemApi<Task, CreateTaskParams, UpdateTaskParams> {
   protected basePath = 'tasks';
   protected entityKey = 'task';
+
+  public bulkCreate(params: BulkCreateTasksParams) {
+    return this.apiClient.makeCall<BulkCreateTasksResponse>(`/${this.basePath}/batch`, 'POST', params);
+  }
 }
