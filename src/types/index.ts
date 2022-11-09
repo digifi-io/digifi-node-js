72caf55baeda33b1cd62bd0f7445e89268252d8d
Katerina Zaitsava <katerina.zaitsava@satelliteinnovations.io>
2022-11-09 18:05:06 +0300

    ND-2524 Add Address variable type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,24 @@
-export type VariableValue = string | boolean | number | null;
+export enum AddressValueKey {
+  StreetName = 'street_name',
+  StreetNumber = 'street_number',
+  Sublocality = 'sublocality',
+  City = 'city',
+  StateOrProvince = 'state_or_province',
+  ZipOrPostalCode = 'zip_or_postal_code',
+  Country = 'country',
+}
+
+export interface AddressValue {
+  [AddressValueKey.StreetNumber]?: string;
+  [AddressValueKey.StreetName]?: string;
+  [AddressValueKey.Sublocality]?: string;
+  [AddressValueKey.City]?: string;
+  [AddressValueKey.StateOrProvince]?: string;
+  [AddressValueKey.ZipOrPostalCode]?: string;
+  [AddressValueKey.Country]?: string;
+}
+
+export type VariableValue = number | string | boolean | AddressValue | null | undefined;
 
 export enum UserRole {
   Owner = 'owner',
