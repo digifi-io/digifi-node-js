688c803e0244c02d70d178965abd779300670133
ilya-tishonok <ilya.tishonok@satelliteinnovations.io>
2022-11-21 22:24:28 +0300

    redo decision engine api service according to new decision engine API

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ export * from './AuthorizedApiClient';
 export * from './api';
 export * from './types';
 export * from './errors';
-export * from './DecisionEngineApiClient';
 export * from './utils/webhook';
 export * from './AuthApiClient';
 export * from './ApplicationDocumentsDownloadsApiClient';
