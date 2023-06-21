d6b503671fc49806bb248a7b38577fc21aee142a
ilya-tishonok <ilya.tishonok@satelliteinnovations.io>
2023-06-21 10:14:51 +0300

    ND-3633 Log request id from response, provide request id in errors

--- src/errors/ApiRequestError.ts
+++ src/errors/ApiRequestError.ts
@@ -1,12 +1,17 @@
 import { Headers } from 'node-fetch';
+import {ResponseHeader} from "../enums/ResponseHeader";
 
 class ApiRequestError extends Error {
+  public readonly requestId: string | null;
+
   constructor(
     message: string,
     readonly responseStatus: number,
     readonly responseHeaders?: Headers,
   ) {
     super(message);
+
+    this.requestId = responseHeaders?.get(ResponseHeader.RequestId) ?? null;
   }
 }
 
